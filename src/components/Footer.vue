44ec5127c25de6869b9c0d0d5da88d68425be368
Marco Antonio Souza Oliveira <marcoantoniosouzaoliveira8@gmail.com>
2023-12-18 23:05:09 -0300

    modificações: adicionei o footer e adicioneir o header na pagina homeView

---
+++ src/components/Footer.vue
@@ -0,0 +1,75 @@
+<template>
+    <footer>
+        <div class="main-footer">
+            <div class="logos">
+                <a target="_blank" href="https://www.facebook.com/"><img src="../assets/facebook.png" alt="Facebook"></a>
+                <a target="_blank" href="https://www.instagram.com/marco_antoniotk/"> <img src="../assets/instagram.png" alt="Instagram"></a>
+                <a target="_blank" href="https://twitter.com/Marcor715"><img src="../assets/twitter.png" alt="Twitter"></a>
+            </div>
+            <div class="info-footer">
+                <span>Info &bull; Suporte &bull; Marketing </span>
+                <span>Teams of Use &bull; Privacy Policy</span>
+                <span class="copy">&copy; 2023 AceCode</span>
+            </div>
+        </div>
+    </footer>
+
+</template>
+
+<script>
+    export default {
+        name:'FooterVue'
+    }
+
+
+</script>
+
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Kanit&display=swap');
+  *{
+    font-family: 'Kanit', sans-serif;
+    box-sizing: border-box;
+  }
+
+    footer {
+        background-image: url(../assets/img6.svg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: bottom;
+        width: 100%;
+        height: 300px;
+        background-color:#051120 ;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    img {
+        width: 40px;
+    }
+    .logos{
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+    }
+    .main-footer{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .info-footer{
+        margin-top: 20px;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 8px;
+
+    }
+
+    .copy{
+        opacity: 0.3;
+    }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,132 @@
 <template>
-<h1>Home</h1>
+
+<main>
+  <header>
+    <div class="container-h1">
+      <h1 >Mensagem, <br> da Mensagem  <br> Mensagem</h1>
+    </div>
+    <img id="imagem-header" src="https://www.kbrtec.com.br/landing-pages/images/criacao-sites-sao-paulo.png" alt="imagem-header">
+  </header>
+  <div class="box-variedades">
+      <div class="variedade1 variedades"> <img src="https://cdn-icons-png.flaticon.com/128/276/276130.png" alt=""></div>
+      <div class="variedade2 variedades"><img src="https://cdn-icons-png.flaticon.com/128/276/276122.png" alt=""></div>
+      <div class="variedade3 variedades"><img src="https://cdn-icons-png.flaticon.com/128/13370/13370080.png" alt=""></div>
+      <div class="variedade4 variedades"> <img src="https://cdn-icons-png.flaticon.com/128/276/276116.png" alt=""></div>
+  </div>
+</main>
+
+<FooterVue></FooterVue>
 </template>
 
 <script>
 // @ is an alias to /src
-
+  import FooterVue from "@/components/Footer.vue"
 
 export default {
   name: 'HomeView',
+  components:{
+    FooterVue
+  },
+  data(){
+    return{
+      nome:"Marco"
+    }
+  }
 
 }
 </script>
+
+
+<style scoped>
+
+@import url(https://db.onlinewebfonts.com/c/99501fdab737541e9315ceaf9229370f?family=Monument+Extended+Regular);
+
+*{
+  font-family: 'Monument Extended Regular', monospace;
+}
+    main{
+      width: 80%;
+      margin: 0% auto;
+
+    }
+
+
+    header{
+      display: flex;
+      background-color: #caeaf9;
+      border-radius:15px ;
+      margin-bottom: 10px;
+    }
+
+    .container-h1{
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .container-h1 h1 {
+      font-size: 3em;
+      font-weight: 100;
+    }
+
+
+    #imagem-header{
+    
+      width: 50%;
+      transform: skewY(15deg);
+
+    }
+
+
+    .box-variedades {
+      border-radius: 15px;
+      margin-bottom: 10px;
+      padding: 10px;
+      width: 100%;
+      
+      display: flex;
+      justify-content: space-around;
+    }
+
+    .variedades {
+      height: 100px;
+      width: 24%;
+      border-radius: 15px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: transform 0.3s;
+      
+    }
+
+    .variedades:hover {
+      cursor: pointer;
+        transform: scale(1.03);
+    }
+
+    .variedades img {
+      width: 30%;
+    }
+
+
+    /*#caeaf9 */
+    .variedade1 {
+      background-color: #58bdec;
+    }
+
+    .variedade2 {
+      background-color: #58ecac;
+    }
+
+    .variedade3 {
+      background-color: #5979ec;
+    }
+
+
+    .variedade4 {
+      background-color: #0097b5;
+    }
+
+
+</style>
